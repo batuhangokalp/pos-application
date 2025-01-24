49645c31e7ee44cbcaf0d437ba56bcfd83058c43
Batuhan Gökalp <bbatuhangokalp@gmail.com>
2025-01-24 12:45:00 +0300

    edited

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -3,8 +3,8 @@ import LoginForm from "../../components/Auth/LoginForm";
 
 const Login = () => {
   return (
-    <div className="h-screen w-full">
-      <div className="flex justify-between h-full w-full">
+    <div className="h-screen w-full overflow-hidden">
+      <div className="h-screen w-full flex flex-col md:flex-row justify-center items-center">
         <LoginForm />
         <CommonCarousel />
       </div>
