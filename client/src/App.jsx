540c891424633f210b2b624238433df18da120f3
Batuhan Gökalp <bbatuhangokalp@gmail.com>
2025-02-20 16:51:16 +0300

    added lazy loading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,39 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import CartPage from "./pages/CartPage";
-import BillPage from "./pages/BillPage";
-import CustomerPage from "./pages/CustomerPage";
-import StatisticsPage from "./pages/StatisticsPage";
-import Register from "./pages/Auth/Register";
-import Login from "./pages/Auth/Login";
+import { lazy, Suspense } from "react";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const CartPage = lazy(() => import("./pages/CartPage"));
+const BillPage = lazy(() => import("./pages/BillPage"));
+const CustomerPage = lazy(() => import("./pages/CustomerPage"));
+const StatisticsPage = lazy(() => import("./pages/StatisticsPage"));
+const Register = lazy(() => import("./pages/Auth/Register"));
+const Login = lazy(() => import("./pages/Auth/Login"));
 
 function App() {
   const storedAuth = JSON.parse(localStorage.getItem("storedUser"));
 
   return (
     <BrowserRouter>
-      <Routes>
-        {!storedAuth && (
-          <>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="*" element={<Navigate to="/login" />} />
-          </>
-        )}
-        {storedAuth && (
-          <>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/bills" element={<BillPage />} />
-            <Route path="/customers" element={<CustomerPage />} />
-            <Route path="/statistics" element={<StatisticsPage />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </>
-        )}
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {!storedAuth ? (
+            <>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="*" element={<Navigate to="/login" />} />
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/bills" element={<BillPage />} />
+              <Route path="/customers" element={<CustomerPage />} />
+              <Route path="/statistics" element={<StatisticsPage />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </>
+          )}
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
